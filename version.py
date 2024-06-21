999ceebed568aa39b738e2667993043e8346afa3
liang-work <152292963+liang-work@users.noreply.github.com>
2024-06-21 20:55:08 +0800

    Add files via upload

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ shuju = {}
 with open("shuju.json",'r') as f:
     shuju = json.load(f)
     f.close()
-print(f'version:{shuju['version']}')
+print(f'version:{shuju["version"]}')
 logging.info(f"[System]: {shuju['version']}")
 os.system("pause")
 logging.info("pause!")
